eaac599a5870692e8f93b83a96af472c949173f2
bagherip <>
2019-12-03 17:13:24 +0100

    Datashader added

--- Datasahder.py
+++ Datasahder.py
@@ -13,7 +13,6 @@ os.chdir("//sbs2003/Daten-CME/")
 t1 = time.time()
 
 data = pd.DataFrame()
-
 # for file in glob.glob("*2013-10*.parquet"):
 for file in glob.glob("*2009-06.csv"):
     df = pd.read_csv(file, usecols=["End_Lon", "End_Lat"])
@@ -22,7 +21,6 @@ for file in glob.glob("*2009-06.csv"):
     data = data.append(df)
     del df
     print("{} added to dataframe".format(file))
-
 print("loading done")
 
 canvas = ds.Canvas(x_range=(-74.25, -73.7), y_range=(40.5, 41), plot_width=4000, plot_height=4000)
