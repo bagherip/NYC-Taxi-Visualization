4a9c7eb554d13081a84661ebd2e0f6ba101d52f1
bagherip <53004649+bagherip@users.noreply.github.com>
2019-12-04 11:11:07 +0100

    Datashader added

--- Datasahder.py
+++ Datasahder.py
@@ -1,6 +1,6 @@
-import glob
 import os
 import time
+import glob
 import datashader as ds
 import pandas as pd
 from colorcet import fire
@@ -14,7 +14,13 @@ t1 = time.time()
 data = pd.DataFrame()
 # for file in glob.glob("*2013-10*.parquet"): # Depending ov the file this line can be activated or deactivated
 for file in glob.glob("*2009-06.csv"):
-    df = pd.read_csv(file, usecols=["End_Lon", "End_Lat"])
+    df = pd.read_csv(
+        file,
+        usecols=[
+            "End_Lon",
+            "End_Lat"
+        ]
+    )
     print(df)
     print("Loaded {}".format(file))
     data = data.append(df)
@@ -22,10 +28,30 @@ for file in glob.glob("*2009-06.csv"):
     print("{} added to dataframe".format(file))
 print("loading done")
 
-canvas = ds.Canvas(x_range=(-74.25, -73.7), y_range=(40.5, 41), plot_width=4000, plot_height=4000)
-agg = canvas.points(data, 'End_Lon', 'End_Lat')
-pic = tf.set_background(tf.shade(agg, min_alpha=200, cmap=fire), "black")
-utils.export_image(pic, "Onefile END", fmt=".png")
+canvas = ds.Canvas(x_range=(-74.25, -73.7),
+                   y_range=(40.5, 41),
+                   plot_width=4000,
+                   plot_height=4000)
+
+agg = canvas.points(data,
+                    'End_Lon',
+                    'End_Lat')
+
+pic = tf.set_background(
+    tf.shade(
+        agg,
+        min_alpha=200,
+        cmap=fire
+    ),
+    "black"
+)
+
+utils.export_image(pic,
+                   "Onefile END",
+                   fmt=".png"
+                   )
 
 print("Image saved!")
-print("Time required:", time.time() - t1)
+print(
+    "Time required:",
+    time.time() - t1)
